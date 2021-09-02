7a08e76801990c2107574bbb38717d502e2a2e15
briannaschladweiler <59517408+briannaschladweiler@users.noreply.github.com>
2021-09-01 20:21:02 -0500

    Add files via upload

---
+++ target/classes/static/capstone 2.js
@@ -0,0 +1,50 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function showAddItem() {
+    var x = document.getElementById("addItemDiv");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function showAddSale() {
+    var x = document.getElementById("addSaleDiv");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function showAddCategory() {
+    var x = document.getElementById("addCategoryDiv");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function showAddSite() {
+    var x = document.getElementById("addSiteDiv");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+    
+function showAddUser() {
+    var x = document.getElementById("addUserDiv");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
